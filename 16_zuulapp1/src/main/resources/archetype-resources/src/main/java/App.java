c9c4df504fb35da74c60e82c3f6a50232d7fffb6
Srinivasan <scalablemind@gmail.com>
2020-10-29 00:47:50 +0800

    added zuul apps

---
+++ 16_zuulapp1/src/main/resources/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.digiterati.zuul.app1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
